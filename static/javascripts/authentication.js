43d9abf6eb0bd6785e5ab24970e4cbf7525e57b5
Arnelle Balane <arnellebalane@gmail.com>
2016-10-12 17:14:08 +0800

    make authentication functions available in global scope

--- static/javascripts/authentication.js
+++ static/javascripts/authentication.js
@@ -1,6 +1,5 @@
 // `authenticated` variable is defined in `chatapp.js` file
 if (!authenticated) {
-
     requestAnimationFrame(_ => $('.signin').addClass('signin--shown'));
 
     // dynamically load the Google platform library
@@ -10,42 +9,50 @@ if (!authenticated) {
     script.defer = true;
     script.onerror = error => console.warn(error);
     document.body.appendChild(script);
+}
 
 
-    function onsignin(googleUser) {
-        const profile = googleUser.getBasicProfile();
-        const user = {
-            name: profile.getName(),
-            avatar: profile.getImageUrl()
-        };
-        login(user)
-            .then(_ => $('.signin').removeClass('signin--shown'))
-            .then(userAuthenticated);
-    }
+/**
+ *  Callback function for when the user has finished signing in to the app
+ *  through the "Sign in with Google" button.
+ **/
+function onsignin(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    const user = {
+        name: profile.getName(),
+        avatar: profile.getImageUrl()
+    };
+    login(user)
+        .then(_ => $('.signin').removeClass('signin--shown'))
+        .then(userAuthenticated);
+}
 
 
-    function login(user) {
-        return fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-        .then(response => response.json())
-        .then(response => {
-            jwtoken = response.jwtoken;
-            localStorage.setItem('jwtoken', jwtoken);
-            authenticated = true;
-            return jwtoken;
-        });
-    }
+/**
+ *  Send user information to server in exchange for a JSON Web Token which will
+ *  then be stored in LocalStorage and used in the `Authentication` header for
+ *  future server requests.
+ **/
+function login(user) {
+    return fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(response => {
+        jwtoken = response.jwtoken;
+        localStorage.setItem('jwtoken', jwtoken);
+        authenticated = true;
+        return jwtoken;
+    });
+}
 
 
-    function logout() {
-        if (!gapi) return null;
-        return gapi.auth2.getAuthInstance().signOut()
-            .then(_ => $('.signin').addClass('signin--shown'));
-    }
-
+function logout() {
+    if (!gapi) return null;
+    return gapi.auth2.getAuthInstance().signOut()
+        .then(_ => $('.signin').addClass('signin--shown'));
 }
